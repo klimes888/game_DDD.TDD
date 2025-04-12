f6da06f7435408d3c5cb68eebbb935027d23c08c
klimes888 <klimes888@gmail.com>
2025-04-12 13:30:05 +0900

    clean file and test code

--- src/user/application/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { UserDto } from '../dto/user.dto';
-
-@Injectable()
-export class UserService {
-  attack(dto: UserDto) {
-    return {};
-  }
-}
